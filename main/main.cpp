0bf09f0770e17d161beb55df13a9d9a8253e1b80
hazyjz <hazyjz@udesk.73-231-152-85.plesk.page>
2023-01-17 18:11:17 -0800

    this main is correct

--- main/main.cpp
+++ main/main.cpp
@@ -1,42 +1,36 @@
-/** ESP32 ShockAbsorber by John Ahearn 2021 */
-    /**
-     * SIMULATION NOTES:
-     *
-    speed at center is amplitude (A) times angular velocity
-    so ... pick A first. something reasonable like 10mm-30mm. let's start with 30mm.
-    anything faster than 15mm/1ms is probably unreasonable, and this is pretty fast as it is.
-    but we can use this as a max and go down from here. OK??
-    for fastest (15mm/ms) ... 30mm * 2 * pi / P = 15mm / 1000us
-    for normal fast (5mm/ms) ... 30mm * 2 * pi / P = 5mm / 1000us
-    for normal  (2mm/ms) ... 30mm * 2 * pi / P = 2mm / 1000us
-    for slow  (0.2mm/ms) ... 30mm * 2 * pi / P = 0.2mm / 1000us
-    Pmin = 2.0 * 2 * pi * 1000 us ~ 12600 us
-    Pfast = 6.0 * 2 * pi * 1000 us ~ 37800 us
-    Pnorm = 15.0 * 2 * pi * 1000 us ~ 94500 us
-    Pslow = 150.0 * 2 * pi * 1000 us ~ 945000 us
-  */
-    /**
-       TEST SANDBOX
-      Pmin @ 15mm = 1.0 * 2 * pi * 1000 us ~ 6300 us
-      Pfast @ 45mm = 9.0 * 2 * pi * 1000 us ~ 56700 us
-      Pnorm @ 30mm = 15.0 * 2 * pi * 1000 us ~ 94500 us
-      Pslow @ 60mm = 300.0 * 2 * pi * 1000 us ~ 1900000 us
-    */
-    /*
-    double zMid1 = Z_MID;
-    double zMidDel1 = 15.0;
-    unsigned long tPeriod1 = 6300L;//this is fast, but estimating not too fast? i'm more concerned about being too slow at the amplitudes
+//file: main.cpp
+#include <LoopManager.h>
 
-    double zMid2 = Z_MID + 10.0;
-    double zMidDel2 = 30.0;
-    unsigned long tPeriod2 = 94500L;//this is fast, but estimating not too fast? i'm more concerned about being too slow at the amplitudes
-
-    double zMid3 = Z_MID - 25.0;
-    double zMidDel3 = 45.0;
-    unsigned long tPeriod3 = 56700L;//this is fast, but estimating not too fast? i'm more concerned about being too slow at the amplitudes
-    */
-#include "LoopManager.h"
 LoopManager* lMan;
-unsigned long tSetupDelay = 2000L;//long because testing OscillatorProfile has a period that's long and if everything starts too soon time deltas go negative
-void setup(){ Serial.begin(115200); delay(tSetupDelay); Serial.println(String(tSetupDelay)+"ms Hello! Here we go!"); lMan = lMan->getInstance("lMan"); }
-void loop(){ lMan->doLoopTask(); }
+void setup(void){
+  using namespace adcspace;
+
+  Serial.begin(115200);
+  pinMode(adcspace::testInputPin1, OUTPUT);
+  delay(Brain::primingDelayMs);
+  Serial.println("");
+  Serial.println("@ main::setup config ADC ... ");
+  adc1_config_width(adcWidth);
+  adc1_config_channel_atten((adc1_channel_t)adcChannel1, adcAtten);
+  adc2_config_channel_atten((adc2_channel_t)adcChannel2, adcAtten);
+  Serial.println("");
+  Serial.println("@ main::setup creating and initializing singletons ... ");
+  lMan = lMan->getInstance();
+}
+
+boolean initializationLoop(void){
+	lMan->doLoopTask();
+	return Brain::containersAndManagersAreInitialized();
+}
+
+void loop(void){
+	lMan->doLoopTask();
+	//lMan->testPulsePins();
+}
+
+extern "C" void app_main(void){
+	initArduino();
+    setup();
+    while (1) { boolean initialized = initializationLoop(); if (initialized) break; }
+    while (1) { loop(); }
+}
